c982c63e39a6083ca934c17d3f52ed48a8f0299a
ideasodaa <nichaphat.id@gmail.com>
2023-04-25 13:15:18 +0700

    change .python_io. to .io.

--- dataset_tool_tf.py
+++ dataset_tool_tf.py
@@ -68,7 +68,7 @@ def main():
     #----------------------------------------------------------
     outdir = os.path.dirname(args.out)
     os.makedirs(outdir, exist_ok=True)
-    writer = tf.python_io.TFRecordWriter(args.out)
+    writer = tf.io.TFRecordWriter(args.out)
     for (idx, imgname) in enumerate(images):
         print (idx, imgname)
         image = load_image(imgname)
